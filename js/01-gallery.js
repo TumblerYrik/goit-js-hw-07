b9dd1968edc2bddaa5ad6ce250246d35bf46ba13
TumblerYrik <151430788+TumblerYrik@users.noreply.github.com>
2024-04-19 21:56:28 +0300

    dz-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,43 +1,55 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// Отримання посилань на елементи DOM
-const galleryContainer = document.querySelector(".gallery");
-// Рендер розмітки галереї
-const galleryMarkup = galleryItems.map(createGalleryItem).join("");
-
 // Шаблон розмітки елемента галереї
 function createGalleryItem({ preview, original, description }) {
-    return `
-    <div class="gallery__item">
+  return `
+    <li class="gallery__item">
       <a class="gallery__link" href="${original}">
         <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
       </a>
-    </div>
+    </li>
   `;
-    instance.show(cb)
 }
 
-galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+// Отримання посилань на елементи DOM
+const galleryContainer = document.querySelector('.gallery');
 
+
+
+// Рендер розмітки галереї
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 // galleryContainer.innerHTML = galleryMarkup;
 
-const lightbox = new SimpleLightbox('.gallery a', {  });
-
 // Реалізація делегування подій і перегляд великого зображення
-galleryContainer.addEventListener("click", handleGalleryClick);
+galleryContainer.addEventListener('click', handleGalleryClick);
 
 function handleGalleryClick(event) {
-    event.preventDefault();
+  event.preventDefault();
+  
+  const clickedElement = event.target;
+  if (clickedElement.nodeName !== 'IMG') {
+    return;
+  }
+  
+  const imageURL = clickedElement.dataset.source;
+  lightbox.open({ items: [{ src: imageURL }] });
+}
 
-    const clickedElement = event.target;
-    if (clickedElement.nodeName !== "IMG") {
-        return;
-    }
+document.addEventListener('keydown', handleKeyPress);
 
-    const imageURL = clickedElement.dataset.source;
-    lightbox.open({ items: [{ src: imageURL }] });
-    
+function handleKeyPress(event) {
+  if (event.code === 'Escape') {
+    lightbox.close();
+  }
 }
 
 
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`);
+
+instance.show(() => console.log("lightbox now visible"));
