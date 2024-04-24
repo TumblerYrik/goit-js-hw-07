d360502aa74861d5ba625c849aa1eb47b8b1d737
TumblerYrik <151430788+TumblerYrik@users.noreply.github.com>
2024-04-24 16:59:57 +0300

    DZ-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,9 +3,15 @@ import { galleryItems } from "./gallery-items.js";
 
 // Отримання посилань на елементи DOM
 const galleryContainer = document.querySelector('.gallery');
-const lightbox = new SimpleLightbox(".gallery a", {
-  /* опції для lightbox */
-});
+
+// Рендер розмітки галереї
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+// galleryContainer.innerHTML = galleryMarkup;
+
+// Реалізація делегування подій і перегляд великого зображення
+galleryContainer.addEventListener('click', handleGalleryClick);
 
 // Шаблон розмітки елемента галереї
 function createGalleryItem({ preview, original, description }) {
@@ -18,29 +24,20 @@ function createGalleryItem({ preview, original, description }) {
   `;
 }
 
-// Рендер розмітки галереї
-const galleryMarkup = galleryItems.map(createGalleryItem).join('');
-
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-// galleryContainer.innerHTML = galleryMarkup;
-
-// Реалізація делегування подій і перегляд великого зображення
-galleryContainer.addEventListener('click', handleGalleryClick);
-
 function handleGalleryClick(event) {
   event.preventDefault();
-  
-  const clickedElement = event.target;
-  if (clickedElement.nodeName !== 'IMG') {
-    return;
-  }
+  console.log(event.target);
+//   const clickedElement = event.target;
+//   if (clickedElement.nodeName !== 'IMG') {
+//     return;
+//   }
 
-  const targetElement = event.target.closest('js-item');
-  const instance = basicLightbox.create(`
-    <img src="data-source" width="800" height="600">
-`);
+//   const targetElement = event.target.closest('js-item');
+//   const instance = basicLightbox.create(`
+//     <img src="data-source" width="800" height="600">
+// `);
 
-  instance.show();
+//   instance.show();
 }
-  const imageURL = clickedElement.dataset.source;
-  lightbox.open({ items: [{ src: imageURL }] });
+//   const imageURL = clickedElement.dataset.source;
+//   lightbox.open({ items: [{ src: imageURL }] });
