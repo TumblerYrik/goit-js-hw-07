b598d463200ac854cb5aab512031956dcdcbaec8
TumblerYrik <151430788+TumblerYrik@users.noreply.github.com>
2024-04-25 21:06:31 +0300

    dz-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,43 +1,40 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// Отримання посилань на елементи DOM
-const galleryContainer = document.querySelector('.gallery');
+const galleryList = document.querySelector('.gallery');
 
-// Рендер розмітки галереї
-const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-// galleryContainer.innerHTML = galleryMarkup;
-
-// Реалізація делегування подій і перегляд великого зображення
-galleryContainer.addEventListener('click', handleGalleryClick);
-
-// Шаблон розмітки елемента галереї
-function createGalleryItem({ preview, original, description }) {
-  return `
-    <li class="gallery__item js-item">
-      <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-      </a>
-    </li>
-  `;
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
 }
+
+galleryList.addEventListener('click', handleGalleryClick);
 
 function handleGalleryClick(event) {
   event.preventDefault();
-  console.log(event.target);
-//   const clickedElement = event.target;
-//   if (clickedElement.nodeName !== 'IMG') {
-//     return;
-//   }
 
-//   const targetElement = event.target.closest('js-item');
-//   const instance = basicLightbox.create(`
-//     <img src="data-source" width="800" height="600">
-// `);
+  const clickedElement = event.target;
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
 
-//   instance.show();
+  const largeImageUrl = clickedElement.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${largeImageUrl}" width="800" height="600">
+`);
+
+  instance.show();
 }
-//   const imageURL = clickedElement.dataset.source;
-//   lightbox.open({ items: [{ src: imageURL }] });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `
+<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        <div class="gallery__caption"> title="${description}"</div> 
+    </a>
+</li>
+      `;
+        })
+        .join("");
+}
+
+// Змінюємо функцію handleGalleryClick
+function handleGalleryClick(event) {
+    event.preventDefault();
+
+    const clickedElement = event.target;
+
+    // Перевіряємо, чи клікнуто на зображення
+    if (clickedElement.classList.contains("gallery__image")) {
+      const largeImageUrl = clickedElement.dataset.source;
+      const imageAlt = clickedElement.alt;
+
+      // Створюємо об'єкт зображення для SimpleLightbox
+      const lightboxImage = {
+        url: largeImageUrl,
+        caption: imageAlt,
+      };
+
+      // Ініціалізуємо SimpleLightbox зі зміненими параметрами
+      const lightbox = new SimpleLightbox(lightboxImage, {
+        captionsData: "alt",
+        captionDelay: 250,
+      });
+
+      // Відкриваємо SimpleLightbox з об'єктом зображення
+      lightbox.open();
+    }
+}
+
+// Додаємо обробник події на ul.gallery
+document.querySelector('.gallery').addEventListener('click', handleGalleryClick);
+
+
